6985a842f0c6dad8dc5145a1edfd01ea3c6ae478
Vijay Kesanakurthi <70144210+VijayKesanakurthi@users.noreply.github.com>
2022-06-27 10:36:17 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Home() {
+  return (
+    <div className='h-screen w-screen bg-[#F8A5C6]'>
+    <Spline scene="https://prod.spline.design/CRoO9zKnzpJa3Equ/scene.splinecode" />
+
+    </div>
+  );
+}
+
